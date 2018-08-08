7ac854b1d788239b71bb63623baa29455ca4919d
Steven Chen <steven.chen313@gmail.com>
2018-08-07 17:56:14 -0700

    Refactor App and Root components, clean up entry file

--- frontend/pintourist.jsx
+++ frontend/pintourist.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-// import Root from './components/root';
-
-// import { signup, login, logout } from './util/session_api_util';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  // window.signup = signup;
-  // window.login = login;
-  // window.logout = logout;
   const store = configureStore();
+
   window.getState = store.getState;
   window.dispatch = store.dispatch;
-  // ReactDOM.render(<Root store={store} />, root);
-  ReactDOM.render(<h1>Welcome to Pintourist!</h1>, root);
+
+  ReactDOM.render(<Root store={store} />, root);
 });
